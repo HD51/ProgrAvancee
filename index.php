1c32574ac05bd64090231c8d0659cdc2ea7ac731
gregodocq <gregory.docq@gmail.com>
2018-02-10 11:43:41 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,13 +3,15 @@ require_once("class/connexion.class.php");
 
 
 
+
 if (isset($_GET['ref'])) {
   $idbd=$_GET['ref'];
-
   $test=new Pdo_Connexion("$idbd");
-
-
   $test->listeTab ($idbd);
+  if (isset($_GET['tab'])) {
+    $table= $_GET['tab'];
+    $test->structab ("$table");
+  }
 }
 else {
   $test=new Pdo_Connexion("");
